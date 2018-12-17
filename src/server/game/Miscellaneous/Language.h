385ba6e983dc152d21f2cee341e7a464228d06d2
ReyDonovan <gennady.zamalaev@gmail.com>
2018-12-17 17:29:50 +0300

    fix build

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1135,10 +1135,7 @@ enum TrinityStrings
     LANG_SHUTDOWN_CANCELLED                       = 11018,
     LANG_YOU_CHANGE_POWER                         = 11019,
     LANG_YOUR_POWER_CHANGED                       = 11020,
-    LANG_INVALID_POWER_NAME                       = 11021
-
-    LANG_LIST_QUESTS                              = 11022,
-    LANG_LIST_QUESTS_DETAIL                       = 11023,
+    LANG_INVALID_POWER_NAME                       = 11021,
 
     //Twin Peaks
     LANG_BG_TP_START_TWO_MINUTES                  = 12000,
